d61b15cdcc384db112ec36273df3ba2f14102703
fvalbuenaelt <david.valbuena@linktic.com>
2024-07-21 23:32:52 -0500

    seccion 6 bloque if

---
+++ src/components/Condicional.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let show = true;  // Variable que controla si se muestra o no el contenido
+</script>
+
+<main>
+    <h1>Condicional</h1>
+    <h2>Mostrar u ocultar contenido</h2>
+    {#if show}
+        <p>Este es el contenido que se muestra</p>
+        {:else}
+            <p>el contenido esta oculto</p>
+    {/if}
+    <button on:click={() => show = !show}>
+        {show ? 'Ocultar' : 'Mostrar'}
+    </button>
+</main>
+
+<style>
+
+</style>
